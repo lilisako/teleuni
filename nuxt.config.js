b8c887a61a135c07bd80ec3687dc91d7fedca7e3
risako <lilisako062397@gmail.com>
2021-10-06 21:08:02 +0900

    update faviccon

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏫</text></svg>' }
     ]
   },
 
